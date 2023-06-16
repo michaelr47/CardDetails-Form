5f987ac5912e082933a0d947395d36685b961d58
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2023-06-16 14:45:45 -0700

    created functions for each input field to check for validity

--- app.js
+++ app.js
@@ -1,25 +1,55 @@
 const confirmButton = document.getElementById('confirmBtn');
+
+const cardNameValidation = name => {
+    return name.length < 0 && isNaN(name);
+}
+
+const cardNumberValidation = number => {
+    return number.length < 16 ? 'Card number should be 16 numbers long' : true;
+}
+
+const cardExpDateValidation = date => {
+    return date.length < 2 || date.length === 0 ? 'invalid exp date' : 'valid exp date';
+}
+
+
 const checkValidity = () => {
     const cardName = document.getElementById('fname').value;
-    const cardNumber = document.getElementById('cardNumber');
-    const expMonth = document.getElementById('expDateMM');
-    const expYear = document.getElementById('expDateYY');
-    const cvc = document.getElementById('CVCInput');
+    const cardNumber = document.getElementById('cardNumber').value;
+    const expMonth = document.getElementById('expDateMM').value;
+    const expYear = document.getElementById('expDateYY').value;
+    const cvc = document.getElementById('CVCInput').value;
+    // console.log(cardName);
+
+    // if (cardNameValidation(cardName)) {
+    //     console.log('invalid name');
+    // } else {
+    //     console.log('good to go :)');
+    //  }
 
 
-    if (nameValid(cardName)) {
-        console.log('once again, did not run!');
-    } else {
-        console.log('good to go :)');
-     }
+    //  if (cardNumberValidation(cardNumber)) {
+    //     console.log('invalid card');
+    //  } else {
+    //     console.log('valid card number')
+    //  }
+
+    //  if (cardExpDateValidation(expMonth)) {
+    //     console.log('invalid exp month')
+    //  } else {
+    //     console.log('valid month');
+    //  }
+
+    //  if (cardExpDateValidation(expYear)) {
+    //     console.log('invalid exp year');
+    //  } else {
+    //     console.log('valid year');
+    //  }
+
+
 } 
 
 
-function nameValid(name) {
-    if (name.length === '') {
-        console.log('did not run!');
-    }
-}
 
 
 confirmButton.addEventListener('click', event => {
