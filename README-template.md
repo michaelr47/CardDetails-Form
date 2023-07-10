0c00ff0c096e537bf6055f8fb0d49145c387a758
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2023-07-09 22:19:25 -0700

    update readme and checking off list

--- README-template.md
+++ README-template.md
@@ -13,8 +13,6 @@ This is a solution to the [Interactive card details form challenge on Frontend M
   - [What I learned](#what-i-learned)
   - [Continued development](#continued-development)
   - [Useful resources](#useful-resources)
-- [Author](#author)
-- [Acknowledgments](#acknowledgments)
 
 **Note: Delete this note and update the table of contents based on what sections you keep.**
 
@@ -45,7 +43,7 @@ Users should be able to:
 ## My process
 
 ### Built with
-
+- vanilla JS
 - Semantic HTML5 markup
 - CSS custom properties
 - Flexbox
--- app.js
+++ app.js
@@ -1,36 +1,34 @@
 /*
+✅
 Attach an event listener to the form submission event.
-
+✅
 Inside the event listener, prevent the default form submission behavior to handle the validation manually.
-
+✅
 Get references to each input field by accessing their DOM elements.
-
+✅
 Create validation functions for each input field to check their respective requirements.
 ✅
 a. For the cardholder name field:
 Ensure that the field is not empty.
 You can optionally validate the format if needed (e.g., only allow alphabets and spaces).
-
+✅
 b. For the card number field:
 Remove any non-digit characters (e.g., spaces, dashes) from the input value.
 Validate the length of the card number (typically 13-16 digits for most card types).
 You can use the Luhn algorithm to perform additional validation if necessary.
-
+✅
 c. For the exp date month field:
 Validate that the value is a number between 1 and 12.
 d. For the exp date year field:
-
+✅
 Validate that the value is a valid two-digit year (e.g., between 00 and 99).
 Optionally, you can check if the year is not in the past.
-
+✅
 e. For the CVC field:
 Validate that the value is a three-digit number.
-Create an array to store the validation results for each input field.
-
-Loop through the array of validation functions and execute each one for the corresponding input field.
-
+✅
 If any validation function returns false, mark the respective input field as invalid and display an error message.
-
+✅
 If all validation functions return true, allow the form submission or proc
 eed with further processing.
 */
